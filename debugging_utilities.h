e834deabe51461e09b5786b527a372ef7ca7076c
REXJJ <rexjj7@gmail.com>
2019-08-19 11:47:05 -0700

    August 19 2019

--- debugging_utilities.h
+++ debugging_utilities.h
@@ -22,79 +22,33 @@
 #define _BCYAN_     "\x1b[1;36;40m"
 #define _BWHITE_    "\x1b[1;37;40m"
 
-extern int DEBUGGING_MODE;
-extern int DEBUG_STOP;
-
-#define dbg_init() int DEBUGGING_MODE=-1,DEBUG_STOP=0;
-
-#define dbg_mod(mode) DEBUGGING_MODE=mode;
-
-#define dbg_stop() DEBUG_STOP=1;
-
-
-#define dbg_active(mode) (DEBUGGING_MODE==mode||DEBUGGING_MODE<0)
-
 #define dbg_color(...) do { printf(__VA_ARGS__); } while(0)
 
 #define dbg(mode,...)                                                    \
     do {                                                                 \
-    	if(dbg_active(mode)){                                            \
     	  printf("DBG: %s:%d %s():",__FILE__,__LINE__,__func__);         \
     	  printf(__VA_ARGS__);                                           \
     	  printf("%s", _NORMAL_);              \
-    	}                                                                \
     } while(0)
 
 #define whereami()                                                       \
     do {                                                                 \
-          if(DEBUG_STOP==0) {                                               \
             dbg_color(_MAGENTA_);                                        \
      	      printf("HERE: %s:%d %s()\n",__FILE__,__LINE__,__func__);       \
      	      dbg_color(_NORMAL_);                                        \
-          }                                                                  \
      	} while(0)
 
 
 
 #define tic() std::chrono::high_resolution_clock::time_point END_TIME,START_TIME=std::chrono::high_resolution_clock::now()
 
-#define tok()                                                       \
-           do {                                                          \
-           	      dbg_color(_GREEN_);                                    \
-                  END_TIME=std::chrono::high_resolution_clock::now();    \
-                  std::cout << " Time elapsed in "<<__func__<<"() : "      \
-                  << std::chrono::duration_cast<std::chrono::microseconds>(END_TIME - START_TIME).count() << " microseconds.\n"; \
-                  dbg_color(_NORMAL_);                                   \
-              }while(0)
-
-#define dbg_n(...)                                                    \
-    do {                                                                 \
-    	  printf("DBG: %s:%d %s():",__FILE__,__LINE__,__func__);         \
-    	  printf(__VA_ARGS__);                                           \
-    	  printf("%s", _NORMAL_);              \
-    } while(0)
-
-#define where()                                                       \
-    do {                                                                 \
-            dbg_color(_MAGENTA_);                                        \
-     	      printf("HERE: %s:%d %s()\n",__FILE__,__LINE__,__func__);       \
-     	      dbg_color(_NORMAL_);                                        \
-     	} while(0)
-
-
-
-#define tik() std::chrono::high_resolution_clock::time_point END_TIME,START_TIME=std::chrono::high_resolution_clock::now()
 
 #define toc()                                                       \
            do {                                                          \
-                  if(DEBUG_STOP==0) {                                    \
            	      dbg_color(_GREEN_);                                    \
                   END_TIME=std::chrono::high_resolution_clock::now();    \
                   std::cout << " Time elapsed in "<<__func__<<"() : "      \
                   << std::chrono::duration_cast<std::chrono::microseconds>(END_TIME - START_TIME).count() << " microseconds.\n"; \
                   dbg_color(_NORMAL_);                                   \
-                }                                                        \
               }while(0)
-
-
 #endif
